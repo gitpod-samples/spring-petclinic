cdaad9664b7b44b0924f0ee5218649a586af0a67
NAGAME711 <i7403340@outlook.com>
2020-06-01 08:11:17 +0000

    test1

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -19,8 +19,10 @@ package org.springframework.samples.petclinic.system;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 class WelcomeController {
 
     @GetMapping("/")
