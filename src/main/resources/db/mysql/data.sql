e8b9cde6007d0030fea02d3f1d9456af1c328077
RYoav <yoav_russo@intuit.com>
2021-11-18 04:22:30 +0000

    final

--- src/main/resources/db/mysql/data.sql
+++ src/main/resources/db/mysql/data.sql
@@ -2,7 +2,7 @@ INSERT IGNORE INTO vets VALUES (1, 'James', 'Carter');
 INSERT IGNORE INTO vets VALUES (2, 'Helen', 'Leary');
 INSERT IGNORE INTO vets VALUES (3, 'Linda', 'Douglas');
 INSERT IGNORE INTO vets VALUES (4, 'Rafael', 'Ortega');
-INSERT IGNORE INTO vets VALUES (5, 'Henry', 'Stevens');
+INSERT IGNORE INTO vets VALUES (5, 'Henry', 'book');
 INSERT IGNORE INTO vets VALUES (6, 'Sharon', 'Jenkins');
 
 INSERT IGNORE INTO specialties VALUES (1, 'radiology');
